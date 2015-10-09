1aa83df2e71091e7c4a29e3d30a004027fd96b77
Martin Halamíček <martin@halamicek.cz>
2015-10-09 13:23:12 +0200

    removed PHP packages

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
 
-default['php']['packages'] = %w{ php56 php56-opcache php56-devel php-pear php56-pdo php56-mysqlnd php56-mbstring php56-mcrypt }
+default['php']['packages'] = %w{ php56 php56-opcache }
 
 default['keboola-maintenance']['reason'] = 'Maintenance'
 default['keboola-maintenance']['estimatedDurationMinutes'] = 15
