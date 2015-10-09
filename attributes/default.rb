0f5a90f2ab21190e1d4a39ac228994096734be52
Martin Halamíček <martin@halamicek.cz>
2015-10-09 13:35:16 +0200

    php packages

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
 
-default['php']['packages'] = %w{ php56 php56-opcache }
+default['php']['packages'] = %w{ php56 php56-opcache php56-devel php-pear php56-mbstring php56-mcrypt }
 
 default['keboola-maintenance']['reason'] = 'Maintenance'
 default['keboola-maintenance']['estimatedDurationMinutes'] = 15
