9da332ca49d3bd7b1eb9f3a8ef10d74a77537f92
Martin Halamíček <martin@halamicek.cz>
2015-10-09 12:26:21 +0200

    connection maintenance

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,5 @@
 
 default['php']['packages'] = %w{ php56 php56-opcache php56-devel php-pear php56-pdo php56-mysqlnd php56-mbstring php56-mcrypt }
+
+default['keboola-maintenance']['reason'] = 'Maintenance'
+default['keboola-maintenance']['estimatedDurationMinutes'] = 15
--- files/default/index.php
+++ files/default/index.php
@@ -1,9 +1,51 @@
-<html>
+<!doctype html>
+<html lang="en">
 	<head>
-		<title>Keboola Generic Server</title>
+		<meta charset="utf-8">
+
+		<title>System is down for maintenance | Keboola Connection</title>
+    <link href="https://d3qqa9wia2dvbu.cloudfront.net/v0.1.78-0-gab62b8b/css/kbc.css" media="screen" rel="stylesheet" type="text/css" >
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&amp;subset=latin,latin-ext" media="screen" rel="stylesheet" type="text/css" >
+    <style type="text/css">
+      .kbc-maintenance .kbc-main-content {
+        padding: 15px;
+      }
+
+      .kbc-maintenance.kbc-single-page {
+        padding-top: 0;
+      }
+      .kbc-maintenance.kbc-single-page>.kbc-navbar {
+        margin-bottom: 0;
+      }
+    </style>
 	</head>
-	<body>
-		<h1>Keboola Generic Server</h1>
-		<p>PHP Version: <?php echo phpversion(); ?>
-	</body>
+	<body class="kbc-single-page kbc-maintenance">
+
+		<nav class="navbar kbc-navbar" role="navigation">
+			<div class="col-xs-10">
+				<div>
+					<div class="kbc-logo col-xs-6">
+						<a href="/admin"><span class="kbc-icon-keboola-logo"></span></a>
+					</div>
+      </div>
+      </div>
+
+		</nav>
+
+		<div class="container-fluid col-xs-10">
+			<h1 class="kbc-title">
+        System is down for maintenance</h1>
+			<div class="kbc-main">
+				<div class="container-fluid kbc-main-content">
+
+  				<div class="col-md-8">
+              <p>The system is down for maintenance.</p>
+              <p> It'll be back <span id="estimatedEndTime">soon</span>.</p>
+              <p>Read more on <a href="http://status.keboola.com" target="_blank">Keboola Status</a></p>
+  				</div>
+			</div>
+		</div>
+  </div>
+
+</body>
 </html>
